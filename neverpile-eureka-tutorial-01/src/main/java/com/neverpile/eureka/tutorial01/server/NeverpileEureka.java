5f77d569e1c9a375c7981b580504495a5b1167ce
Kevin Hertfelder <k.hertfelder@levigo.de>
2019-12-30 18:32:23 +0100

    Upgrade to java 11.

--- neverpile-eureka-tutorial-01/src/main/java/com/neverpile/eureka/tutorial01/server/NeverpileEureka.java
+++ neverpile-eureka-tutorial-01/src/main/java/com/neverpile/eureka/tutorial01/server/NeverpileEureka.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.neverpile.eureka.util.EnableNeverpileEurekaSpringApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @SpringBootApplication
 @EnableNeverpileEurekaSpringApplication
 public class NeverpileEureka {
